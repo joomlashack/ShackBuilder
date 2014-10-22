8b5b16015f718a68b80f445fbb31089d68873dcd
Anderson Grüdtner Martins <anderson@ostraining.com>
2014-10-22 00:58:57 -0200

    Fix to build using custom projects path

--- src/tasks/CheckBranchTask.php
+++ src/tasks/CheckBranchTask.php
@@ -11,28 +11,11 @@ require_once 'phing/Task.php';
 class CheckBranchTask extends Task
 {
     /**
-     * Projects path, where we will look for projects
-     *
-     * @var string
-     */
-    protected $projectsPath;
-
-    /**
      * The project list to check, as CSV
      *
      * @var array
      */
     protected $projects;
-
-    /**
-     * Set the project path
-     *
-     * @param string $path
-     */
-    public function setProjectPath($path)
-    {
-        $this->projectsPath = $path;
-    }
 
     /**
      * Set the project list, converting the string to array
@@ -51,20 +34,30 @@ class CheckBranchTask extends Task
      */
     public function main()
     {
+        $properties = $this->project->getProperties();
+
+        // Get the projects path
+        $paths = array();
+        foreach ($properties as $property => $value) {
+            if (preg_match('/^project\.([a-z0-9_\-]+)\.path/i', $property, $matches)) {
+                $paths[$matches[1]] = $value;
+            }
+        }
+
         $hasNonMaster = false;
         $offset = str_repeat(' ', 14);
         $output = "=================[ Branches ]=================\n\n";
 
         if (isset($this->projects)) {
             foreach ($this->projects as $project) {
-                $headFilePath = realpath($this->projectsPath . '/' . $project . '/.git/HEAD');
+                $headFilePath = realpath($paths[$project] . '/.git/HEAD');
 
                 if (is_file($headFilePath)) {
                     $head = file_get_contents($headFilePath);
 
                     if (!empty($head)) {
-                        preg_match('/heads\/(.*)/', $head, $matchs);
-                        $branch = $matchs[1];
+                        preg_match('/heads\/(.*)/', $head, $matches);
+                        $branch = $matches[1];
 
                         $output .= $offset . str_pad($project, 20, ' ') . ': ' . $branch;
 
