861cd881a557a8fe81060583a05f365e75b220ee
Anderson Grudtner Martins <anderson@ostraining.com>
2015-03-03 17:54:20 -0300

    Fixes installer tests for J2.5

--- src/codeception/acceptance/ExtensionInstallerAbstractCest.php
+++ src/codeception/acceptance/ExtensionInstallerAbstractCest.php
@@ -14,9 +14,16 @@ abstract class ExtensionInstallerAbstractCest
     protected function loginIntoAdmin(AcceptanceTester $I)
     {
         $I->amOnUrl(TEST_HOST_BASEURL . '/administrator/index.php');
-        $I->fillField('username', 'admin');
-        $I->fillField('passwd', 'admin');
-        $I->click('//*[@id="form-login"]/fieldset/div[3]/div/div/button');
+
+        if (version_compare(JVERSION, '3.0', 'lt')) {
+            $I->fillField('username', 'admin');
+            $I->fillField('passwd', 'admin');
+            $I->click('//*[@id="form-login"]/fieldset/div[1]/div/div/a');
+        } else {
+            $I->fillField('username', 'admin');
+            $I->fillField('passwd', 'admin');
+            $I->click('//*[@id="form-login"]/fieldset/div[3]/div/div/button');
+        }
     }
 
     /**
@@ -25,10 +32,18 @@ abstract class ExtensionInstallerAbstractCest
     public function installExtension(AcceptanceTester $I)
     {
         $I->amOnUrl(TEST_HOST_BASEURL . '/administrator/index.php?option=com_installer');
-        $I->click('Install from Directory');
-        $I->fillField('//*[@id="install_directory"]', '/project/tests/_output/extension_installer');
-        $I->click('//*[@id="directory"]/fieldset/div[2]/input');
-        $I->dontSeeElementInDOM(['css' => '#system-message-container div.alert-error']);
-        $I->seeElementInDOM(['css' => '#system-message-container div.alert-success']);
+
+        if (version_compare(JVERSION, '3.0', 'lt')) {
+            $I->fillField('//*[@id="install_directory"]', '/project/tests/_output/extension_installer');
+            $I->click('//*[@id="adminForm"]/div/fieldset[2]/input[2]');
+            $I->seeElementInDOM(['css' => '#system-message dd.message.message']);
+            $I->dontSeeElementInDOM(['css' => '#system-message dd.message.error']);
+        } else {
+            $I->click('Install from Directory');
+            $I->fillField('//*[@id="install_directory"]', '/project/tests/_output/extension_installer');
+            $I->click('//*[@id="directory"]/fieldset/div[2]/input');
+            $I->dontSeeElementInDOM(['css' => '#system-message-container div.alert-error']);
+            $I->seeElementInDOM(['css' => '#system-message-container div.alert-success']);
+        }
     }
 }
